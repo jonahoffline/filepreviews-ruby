f5519a53504766988b2e46813cb5332f0ccd29df
Jonah Ruiz <jonah@pixelhipsters.com>
2015-07-23 22:31:52 -0400

    Add :secret_key to configure block

--- lib/filepreviews/config.rb
+++ lib/filepreviews/config.rb
@@ -6,20 +6,22 @@ module Filepreviews
       base.extend(self)
     end
 
-    # @!attribute api_key
-    # @return [String] API key to be used
-    attr_accessor :api_key
+    # @!attribute api_key, :secret_key
+    # @return [String] API/Secret key to be used
+    attr_accessor :api_key, :secret_key
 
     # Configures api_key and options
     #   Usage example:
     #     Filepreviews.configure do |config|
     #       config.api_key = 'your_api_key_here'
+    #       config.secret_key = 'your_api_key_here'
     #     end
     #
     #   Alternate way:
     #     Filepreviews.api_key = ENV['YOUR_API_KEY']
+    #     Filepreviews.secret_key = ENV['YOUR_SECRET_KEY']
     #
-    # @param <api_key> [String] api key to use
+    # @param <api_key> [String] api/secret key to use
     def configure
       yield self if block_given?
     end
